a08155749684be7cd8cb7923025773cfa248fc60
ninomiyatakashi <ionstyledebut3@gmail.com>
2012-08-29 11:55:46 +0900

    first commit

---
+++ Ex8_3.java
@@ -0,0 +1,25 @@
+package Excercise;
+
+public class Ex8_3 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		int i =1;
+		int total = 0;
+		
+		while(i<=10){
+			
+			total += +i;
+			
+			i++;
+		}
+		
+		System.out.println("1～10の合計=" + total);
+		// TODO Auto-generated method stub
+
+	}
+
+}
